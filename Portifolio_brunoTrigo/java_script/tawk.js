aea1ece43118de038a06a7b67f5d96bcd1c5fb1a
Bruno Trigo <seu-email-do-github@exemplo.com>
2026-09-15 19:19:35 -0300

    Adiciona chat Tawk ao portfolio

---
+++ Portifolio_brunoTrigo/java_script/tawk.js
@@ -0,0 +1,12 @@
+(function () {
+    var Tawk_API = window.Tawk_API = window.Tawk_API || {};
+    window.Tawk_LoadStart = new Date();
+
+    var tawkScript = document.createElement("script");
+    var firstScript = document.getElementsByTagName("script")[0];
+    tawkScript.async = true;
+    tawkScript.src = "https://embed.tawk.to/6aa85f5588ca00344d721cc5/default";
+    tawkScript.charset = "UTF-8";
+    tawkScript.setAttribute("crossorigin", "*");
+    firstScript.parentNode.insertBefore(tawkScript, firstScript);
+})();
